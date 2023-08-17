f1ce7fea1685dd44db6c5413823029d3afa78bbb
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-17 16:01:44 -0400

    Add dockerfile

--- server/Program.cs
+++ server/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+DotNetEnv.Env.Load();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
