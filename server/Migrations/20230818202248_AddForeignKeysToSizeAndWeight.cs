eb5dada8675b397386b9bb064657eac61e0deba4
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-18 16:23:39 -0400

    Add foreign keys to Size and Weight

---
+++ server/Migrations/20230818202248_AddForeignKeysToSizeAndWeight.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeysToSizeAndWeight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemon_SizeId",
+                table: "Pokemon",
+                column: "SizeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemon_WeightId",
+                table: "Pokemon",
+                column: "WeightId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemon_Size_SizeId",
+                table: "Pokemon",
+                column: "SizeId",
+                principalTable: "Size",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemon_Weight_WeightId",
+                table: "Pokemon",
+                column: "WeightId",
+                principalTable: "Weight",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemon_Size_SizeId",
+                table: "Pokemon");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemon_Weight_WeightId",
+                table: "Pokemon");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pokemon_SizeId",
+                table: "Pokemon");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pokemon_WeightId",
+                table: "Pokemon");
+        }
+    }
+}
