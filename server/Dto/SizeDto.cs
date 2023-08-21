2cd0110bba39386007c0b18857ce2d76ae73af1d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-21 15:31:39 -0400

    Add SizeDto

---
+++ server/Dto/SizeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Dto
+{
+    public class SizeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Space { get; set; }
+        public ICollection<PokemonResourceDto> Pokemon { get; set; }
+    }
+}
