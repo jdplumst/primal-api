29759f57378c543c723ea01a4cc6b484cf389c98
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-22 15:58:27 -0400

    Add InfoDto

---
+++ server/Dto/InfoDto.cs
@@ -0,0 +1,21 @@
+﻿namespace server.Dto
+{
+    public class InfoDto
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string NextPage { get; set; }
+        public string PreviousPage { get; set; }
+
+        public InfoDto()
+        {
+
+        }
+
+        public InfoDto(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
