1468c3fbda51c0d22fc959a513395bee32cec71d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-22 15:58:40 -0400

    Add PageDto

---
+++ server/Dto/PageDto.cs
@@ -0,0 +1,19 @@
+﻿namespace server.Dto
+{
+    public class PageDto<T>
+    {
+        public PageDto()
+        {
+
+        }
+
+        public PageDto(T data, InfoDto info)
+        {
+            Data = data;
+            Info = info;
+
+        }
+        public InfoDto Info { get; set; }
+        public T Data { get; set; }
+    }
+}
