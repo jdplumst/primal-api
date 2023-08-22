1dd7345ff5e8c3af4c145b66bd34280ec8c4996a
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-22 15:58:55 -0400

    Add PaginationQuery

---
+++ server/Queries/PaginationQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace server.Queries
+{
+    public class PaginationQuery
+    {
+        public PaginationQuery()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+
+        public PaginationQuery(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
