ec3e6008b349dad7fcdb3855661da6c74c4a340c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-24 18:28:35 -0400

    Add Habitat Dto

---
+++ server/Dto/HabitatDto.cs
@@ -0,0 +1,19 @@
+﻿namespace server.Dto
+{
+    public class HabitatDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<ResourceDto> Pokemon { get; set; }
+
+        public HabitatDto(int id, string name, string description, ICollection<ResourceDto> pokemon)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Pokemon = pokemon;
+
+        }
+    }
+}
