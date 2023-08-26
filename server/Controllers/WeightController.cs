a595e3d9c603f2490b45840b5cecf0cfcafd417c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-26 10:49:15 -0400

    Add Get Weight By Id endpoint

---
+++ server/Controllers/WeightController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using server.Dto;
+using server.Interfaces;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/api/weight")]
+    public class WeightController : ControllerBase
+    {
+        private readonly IWeightRepository _weightRepository;
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IResourceMaker _resourceMaker;
+
+        public WeightController(IWeightRepository weightRepository, IPokemonRepository pokemonRepository, IResourceMaker resourceMaker)
+        {
+            _weightRepository = weightRepository;
+            _pokemonRepository = pokemonRepository;
+            _resourceMaker = resourceMaker;
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(200, Type = typeof(WeightDto))]
+        public IActionResult GetWeightById(int id)
+        {
+            var weight = _weightRepository.GetWeightById(id);
+            if (weight == null)
+            {
+                return NotFound();
+            }
+            var pokemon = _pokemonRepository.GetPokemonByWeightId(id);
+            var pokemonList = _resourceMaker.CreatePokemonResources(pokemon);
+            return Ok(new WeightDto(id, weight.Name, weight.Range, pokemonList));
+        }
+    }
+}
