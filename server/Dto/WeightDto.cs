3c87d65111f248b9249e58192966a35292dfe98d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-26 10:32:12 -0400

    Add Weight Dto

---
+++ server/Dto/WeightDto.cs
@@ -0,0 +1,18 @@
+﻿namespace server.Dto
+{
+    public class WeightDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Range { get; set; }
+        public ICollection<ResourceDto> Pokemon { get; set; }
+
+        public WeightDto(int id, string name, string range, ICollection<ResourceDto> pokemon)
+        {
+            Id = id;
+            Name = name;
+            Range = range;
+            Pokemon = pokemon;
+        }
+    }
+}
