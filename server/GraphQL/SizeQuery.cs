47c97eef069e874fd94bbee891b22044b495e82d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-08-27 13:01:10 -0400

    Add Get Size By Name GraphQL Query

--- server/GraphQL/SizeQuery.cs
+++ server/GraphQL/SizeQuery.cs
@@ -13,16 +13,35 @@ namespace PrimalAPI.GraphQL
             IResourceMaker resourceMaker,
             [Service] ILogger<SizeQuery> logger)
         {
-            logger.LogInformation($"Getting Size by Id {id}");
+            logger.LogInformation($"Getting Size by Id {id} (GraphQL)");
             var size = sizeRepository.GetSizeById(id);
             if (size == null)
             {
-                logger.LogWarning($"Size with Id {id} was not found");
+                logger.LogWarning($"Size with Id {id} was not found (GraphQL)");
                 return null;
             }
             var pokemon = pokemonRepository.GetPokemonBySizeId(id);
             var pokemonList = resourceMaker.CreatePokemonResources(pokemon);
             return new SizeDto(id, size.Name, size.Space, pokemonList);
+        }
+
+        public SizeDto? GetSizeByName(
+            string name,
+            ISizeRepository sizeRepository,
+            IPokemonRepository pokemonRepository,
+            IResourceMaker resourceMaker,
+            [Service] ILogger<SizeQuery> logger)
+        {
+            logger.LogInformation($"Getting Size by Name {name} (GraphQL)");
+            var size = sizeRepository.GetSizeByName(name);
+            if (size == null)
+            {
+                logger.LogWarning($"Size with Name {name} was not found (GraphQL)");
+                return null;
+            }
+            var pokemon = pokemonRepository.GetPokemonBySizeName(name);
+            var pokemonList = resourceMaker.CreatePokemonResources(pokemon);
+            return new SizeDto(size.Id, size.Name, size.Space, pokemonList);
         }
 
     }
