b79760edac7534847828b69194137fa86c8cbaa2
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-09-10 11:19:50 -0400

    Add Rarity GraphQL Dto

---
+++ server/GraphQL/Dto/RarityGraphQLDto.cs
@@ -0,0 +1,36 @@
+﻿using PrimalAPI.Interfaces;
+
+namespace PrimalAPI.GraphQL.Dto
+{
+    [GraphQLName("Rarity")]
+    public class RarityGraphQLDto
+    {
+        [GraphQLDescription("The id of the rarity")]
+        public int Id { get; set; }
+
+        [GraphQLDescription("The name of the rarity")]
+        public string Name { get; set; }
+
+        [GraphQLDescription("A description of the rarity")]
+        public string Description { get; set; }
+
+        [GraphQLDescription("A list of Pokemon with this rarity")]
+        public ICollection<PokemonGraphQLDto> GetPokemon([Parent] RarityGraphQLDto rarity, IPokemonRepository pokemonRepository)
+        {
+            var pokemons = pokemonRepository.GetPokemonByRarityId(rarity.Id);
+            var pokemonList = new List<PokemonGraphQLDto>();
+            foreach (var pokemon in pokemons)
+            {
+                pokemonList.Add(new PokemonGraphQLDto(pokemon.Id, pokemon.Name, pokemon.HP, pokemon.Attack, pokemon.SpecialAttack, pokemon.Defense, pokemon.SpecialDefense, pokemon.Speed, pokemon.HatchRate));
+            }
+            return pokemonList;
+        }
+
+        public RarityGraphQLDto(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
