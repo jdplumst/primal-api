40544412e9ed2c95340ab644c334562edf4b778f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-09-12 15:56:00 -0400

    Clean up Pokemon Repository code

--- server/Repositories/PokemonRepository.cs
+++ server/Repositories/PokemonRepository.cs
@@ -14,51 +14,78 @@ namespace PrimalAPI.Repositories
 
         public ICollection<Pokemon> GetPokemonBySizeId(int sizeId)
         {
-            return _context.Pokemon.Where(p => p.SizeId == sizeId).OrderBy(p => p.Id).ToList();
+            return _context.Pokemon
+                .Where(p => p.SizeId == sizeId)
+                .OrderBy(p => p.Id)
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonBySizeName(string sizeName)
         {
-            var sizes = _context.Size.Where(s => s.Name.ToLower() == sizeName.ToLower()).Select(p => p.Id);
-            return _context.Pokemon.Where(p => sizes.Contains(p.SizeId)).ToList();
+            var sizes = _context.Size
+                .Where(s => s.Name.ToLower() == sizeName.ToLower())
+                .Select(p => p.Id);
+            return _context.Pokemon
+                .Where(p => sizes.Contains(p.SizeId))
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByHabitatId(int habitatId)
         {
-            return _context.Pokemon.Where(p => p.Habitat.Any(h => h.Id == habitatId)).OrderBy(p => p.Id).ToList();
+            return _context.Pokemon
+                .Where(p => p.Habitat.Any(h => h.Id == habitatId))
+                .OrderBy(p => p.Id)
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByHabitatName(string habitatName)
         {
-            return _context.Pokemon.Where(p => p.Habitat.Any(h => h.Name.ToLower() == habitatName.ToLower())).OrderBy(p => p.Id).ToList();
+            return _context.Pokemon
+                .Where(p => p.Habitat.Any(h => h.Name.ToLower() == habitatName.ToLower()))
+                .OrderBy(p => p.Id)
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByWeightId(int weightId)
         {
-            return _context.Pokemon.Where(p => p.WeightId == weightId).OrderBy(p => p.Id).ToList();
+            return _context.Pokemon
+                .Where(p => p.WeightId == weightId)
+                .OrderBy(p => p.Id)
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByWeightName(string weightName)
         {
-            var weights = _context.Weight.Where(w => w.Name.ToLower() == weightName.ToLower()).Select(w => w.Id);
-            return _context.Pokemon.Where(p => weights.Contains(p.WeightId)).ToList();
+            var weights = _context.Weight
+                .Where(w => w.Name.ToLower() == weightName.ToLower())
+                .Select(w => w.Id);
+            return _context.Pokemon
+                .Where(p => weights.Contains(p.WeightId))
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByRarityId(int rarityId)
         {
-            return _context.Pokemon.Where(p => p.RarityId == rarityId).OrderBy(p => p.Id).ToList();
+            return _context.Pokemon
+                .Where(p => p.RarityId == rarityId)
+                .OrderBy(p => p.Id)
+                .ToList();
         }
 
         public ICollection<Pokemon> GetPokemonByEggGroupId(int eggGroupId)
         {
-            return _context.Pokemon.Where(p => p.EggGroup.Any(e => e.Id == eggGroupId)).ToList();
+            return _context.Pokemon
+                .Where(p => p.EggGroup.Any(e => e.Id == eggGroupId))
+                .ToList();
         }
 
         public ICollection<Pokemon> GetRandomPokemonFromHabitat(string habitatName, int count)
         {
             return _context.Pokemon
                 .Where(p => p.Habitat.Any(h => h.Name.ToLower() == habitatName.ToLower()))
-                .OrderBy(p => Guid.NewGuid()).Take(count).ToList();
+                .OrderBy(p => Guid.NewGuid())
+                .Take(count)
+                .ToList();
         }
     }
 }
