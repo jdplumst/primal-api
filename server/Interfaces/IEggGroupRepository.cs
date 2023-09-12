992462e49878aff68b6d875b64e976736194b579
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-09-12 15:19:29 -0400

    Add Egg Group Repository

---
+++ server/Interfaces/IEggGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using PrimalAPI.Models;
+using PrimalAPI.Queries;
+
+namespace PrimalAPI.Interfaces
+{
+    public interface IEggGroupRepository
+    {
+        EggGroup? GetEggGroupById(int eggGroupId);
+        EggGroup? GetEggGroupByName(string eggGroupName);
+        ICollection<EggGroup> GetEggGroups(PaginationQuery paginationQuery);
+        int GetEggGroupCount();
+        ICollection<EggGroup> GetEggGroupByPokemonId(int pokemonId);
+        ICollection<EggGroup> GetAllEggGroups();
+    }
+}
---
+++ server/Repositories/EggGroupRepository.cs
@@ -0,0 +1,47 @@
+﻿using PrimalAPI.Interfaces;
+using PrimalAPI.Models;
+using PrimalAPI.Queries;
+
+namespace PrimalAPI.Repositories
+{
+    public class EggGroupRepository : IEggGroupRepository
+    {
+        private readonly DataContext _context;
+
+        public EggGroupRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public EggGroup? GetEggGroupById(int eggGroupId)
+        {
+            return _context.EggGroup.Where(e => e.Id == eggGroupId).FirstOrDefault();
+        }
+
+        public EggGroup? GetEggGroupByName(string eggGroupName)
+        {
+            return _context.EggGroup.Where(e => e.Name.ToLower() == eggGroupName.ToLower()).FirstOrDefault();
+        }
+
+        public ICollection<EggGroup> GetEggGroups(PaginationQuery paginationQuery)
+        {
+            var skip = (paginationQuery.PageNumber - 1) * paginationQuery.PageSize;
+            return _context.EggGroup.OrderBy(e => e.Id).Skip(skip).Take(paginationQuery.PageSize).ToList();
+        }
+
+        public int GetEggGroupCount()
+        {
+            return _context.EggGroup.Count();
+        }
+
+        public ICollection<EggGroup> GetEggGroupByPokemonId(int pokemonId)
+        {
+            return _context.EggGroup.Where(e => e.Pokemon.Any(p => p.Id == pokemonId)).ToList();
+        }
+
+        public ICollection<EggGroup> GetAllEggGroups()
+        {
+            return _context.EggGroup.OrderBy(e => e.Id).ToList();
+        }
+    }
+}
