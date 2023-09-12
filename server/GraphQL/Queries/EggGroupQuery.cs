bc4073ed78bff5ea96278bc98c2cc472ec989f7b
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-09-12 15:34:00 -0400

    Add Get Egg Group By Id Query (GraphQL)

---
+++ server/GraphQL/Queries/EggGroupQuery.cs
@@ -0,0 +1,25 @@
+﻿using PrimalAPI.GraphQL.Dto;
+using PrimalAPI.Interfaces;
+
+namespace PrimalAPI.GraphQL.Queries
+{
+    [ExtendObjectType("Query")]
+    public class EggGroupQuery
+    {
+        [GraphQLDescription("Get a specific egg group by id")]
+        public EggGroupGraphQLDto? GetEggGroupById(
+            int id,
+            IEggGroupRepository eggGroupRepository,
+            [Service] ILogger<EggGroupQuery> logger)
+        {
+            logger.LogInformation($"Getting Egg Group by Id {id} (GraphQL)");
+            var eggGroup = eggGroupRepository.GetEggGroupById(id);
+            if (eggGroup == null)
+            {
+                logger.LogWarning($"Egg Group with Id {id} was not found (GraphQL)");
+                return null;
+            }
+            return new EggGroupGraphQLDto(eggGroup.Id, eggGroup.Name);
+        }
+    }
+}
